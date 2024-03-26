789951a51615e7734f17ecac1d27e14b89803d74
Aditya Dahake <97397447+adityadahake24@users.noreply.github.com>
2024-03-27 02:24:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>CNCF Nagpur</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Cloud Natve Nagpur!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
